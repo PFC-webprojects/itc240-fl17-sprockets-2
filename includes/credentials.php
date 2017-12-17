3ab27fb98a8f24d31fb28be78c21eed916c8d284
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2017-12-17 04:44:20 -0800

    Delete credentials.php

--- includes/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','pfc_webprojects_db');
-define('DB_USER','pfc_wbprjcts_adm');
-define('DB_PASSWORD','MjS-H2^2');
-define('DB_HOST','mysql.pfc-webprojects.zone');
